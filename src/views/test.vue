45fca0f80d5bffb3f2b069f83125046d955dab53
dulei <dul>
2018-12-21 16:56:38 +0800

    -

--- src/views/test.vue
+++ src/views/test.vue
@@ -121,7 +121,7 @@ export default {
       });
     },
     NodeDel(n, d) {
-      //删除节点ssss
+      //删除节点sssssss
       console.log(n, d);
       //let that = this;
       if (d.children && d.children.length !== 0) {
